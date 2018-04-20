fcbb081f64c41a4e2420747dd6585464f077ae62
Clarice Bianchini Barreto da Costa <cbbcosta@users.noreply.github.com>
2018-04-20 14:32:11 -0300

    Create function that defines Jokenpo Judge's behavior

---
+++ dojo-girls.py
@@ -0,0 +1,8 @@
+def jokenpo(j1, j2):
+    if j1 == 'tesoura' and j2 == 'papel':
+        print 'j1 ganhou'
+
+j1 = raw_input('Jogador 1: Escolha entre pedra, papel ou tesoura:')
+j2 = raw_input('Jogador 2: Escolha entre pedra, papel ou tesoura:')
+
+jokenpo(j1, j2)
